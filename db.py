a50ae8642d5bce0efcd84943e15af32011029d34
Sigfrid0109 <sigfridmorales6@gmail.com>
2025-11-10 22:44:15 -0600

    Reconfigura conexión segura con Aiven

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
+import os
 import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host="makiroll-restaurantemaki.k.aivencloud.com",
-        user="avnadmin",
-        password="AVNS_LdNXgEsqQmTBRwJGAeq",
-        database="defaultdb",
-        port=25304,
+        host=os.getenv("MYSQL_HOST", "makiroll-restaurantemaki.k.aivencloud.com"),
+        user=os.getenv("MYSQL_USER", "avnadmin"),
+        password=os.getenv("MYSQL_PASSWORD", "AVNS_LdNXgEsqQmTBRwJGAeq"),
+        database=os.getenv("MYSQL_DB", "defaultdb"),
+        port=int(os.getenv("MYSQL_PORT", 25304)),
         ssl_disabled=False
     )
